0c002de8ef0937bef2e0c1c0ef9fa1da9319488b
isaka2025 <bouvetproj@gmail.com>
2026-03-01 13:21:47 +0100

    Add support for trophies

--- src/asm/hooks.s
+++ src/asm/hooks.s
@@ -5,10 +5,14 @@
 .endm 
 
 # --- Entrypoints ---
-# Asdfasdfasdfasdf
+# Hookpoint
 PATCH 0x97cba4, cellPadSetActDirect, bl hook
 
 # --- Patches ---
 # Skip intro, I use it to boot the game faster on RPCS3 mainly.
 PATCH 0x1113c, introskip, b 0x4b8
 PATCH 0x9027a0, modeLobbyInit_skip, blr
+
+# The game runs periodic checks to make sure that the in-game trophy unlock status is synced with the console
+# So we need to nop this instruction to make it not reset our custom trophy unlocks.
+PATCH 0x985318, dontResetTrophyStatus, nop
